56cdd77dcacf2421633666de38c8e9c174247eb9
Jowel Castaneda <jowell@kodego.ph>
2022-08-26 19:19:59 +0800

    authentication setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import TodoList from './components/TodoList';
+import Auth from './components/authentication/Auth';
 
 function App() {
   return (
     <div className="App">
-      <TodoList />
+      <Auth />
     </div>
   );
 }
