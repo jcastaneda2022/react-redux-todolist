fb1933a40f52d1caaf1ad9973c84e36e820463ae
Jowel Castaneda <jowell@kodego.ph>
2022-08-27 09:19:32 +0800

    Update registerReducer.js

--- src/redux/reducers/registerReducer.js
+++ src/redux/reducers/registerReducer.js
@@ -1,4 +1,4 @@
-const initialState = [{ username: 'test1', email: 'testemail@kodego.com', password: 123 }];
+const initialState = [];
 
 const registerReducer = (state = initialState, action) => {
     switch (action.type) {
